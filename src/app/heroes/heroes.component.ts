f0e537077e55ca54ecb2ba1a88e736f141c82f0f
Pedro Ferreira da Silva <pedroca-2003@hotmail.com>
2022-01-20 13:19:20 -0300

    commit inicial

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss'],
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Homem-Aranha',
+  };
+  heroes = HEROES;
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Wolverine'},
+    { id: 2, name: 'Homem-Aranha'},
+    { id: 3, name: 'Hulk'},
+    { id: 4, name: 'Homem de Ferro'},
+    { id: 5, name: 'Pantera Negra'},
+    { id: 6, name: 'Capitão América'},
+]
